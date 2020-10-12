860b4e9a0adc9298cfeafd153c958c43caf39840
Nroche24 <71049417+Nroche24@users.noreply.github.com>
2020-10-11 20:02:39 -0400

    Temp outcomes

---
+++ conditions.py
@@ -0,0 +1,25 @@
+myvar = "Temp"
+Temp = int (input ("Please enter current tempature:"))
+#Loop Begins
+if Temp > 90 :
+        print ("Wear shorts")
+elif Temp>70:
+        print ("Short sleeves are fine")
+
+elif Temp>50:
+        print ("Wear a Jacket")
+
+elif Temp>32:
+        print ("Wear a heavy coat")
+
+else:   print ("Stay Inside")
+
+#End Loop
+
+print ("The End") 
+
+
+        
+
+
+
