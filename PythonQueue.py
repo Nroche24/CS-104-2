57ed091f848da5ee76cbe34fd9e902c2c4fd9a76
Nroche24 <71049417+Nroche24@users.noreply.github.com>
2020-11-25 15:55:17 -0500

    Add files via upload
    Python Queue Simulation

---
+++ PythonQueue.py
@@ -0,0 +1,16 @@
+names = ["Neal","Jim","Tim","John","Sally","Joe","Chris","Christina","Frank","Tony"]
+y =(names) 
+acceptedname= "Neal"
+for A in range(10):
+    y = input('Enter a name: ')
+    names.append(acceptedname)
+
+
+
+
+
+
+
+for B in range(10):
+    len(names) 
+    print (names.pop(0))
